7f1554a5cad014180d4ee8243fd191451baf8ba0
toshikazu.ohashi <toshikazu.ohashi@iridge.jp>
2018-05-07 19:25:08 +0900

    Add add command implements

--- cmd/add.go
+++ cmd/add.go
@@ -16,7 +16,12 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
 
+	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 )
 
@@ -30,9 +35,7 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
-	},
+	RunE: add,
 }
 
 func init() {
@@ -48,3 +51,78 @@ func init() {
 	// is called directly, e.g.:
 	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
+
+func add(cmd *cobra.Command, args []string) error {
+	fmt.Println("add called")
+
+	targetTime := time.Now()
+	targetPath, err := diaryPath(targetTime, diaryDirPath())
+	if err != nil {
+		return err
+	}
+
+	// Make directory
+	targetDirPath := filepath.Dir(targetPath)
+	if !isFileExist(targetDirPath) {
+		if err := os.MkdirAll(targetDirPath, 0755); err != nil {
+			return fmt.Errorf("Failed make diary dir. %s", err.Error())
+		}
+	}
+
+	// // Make diary file
+	// if !isFileExist(targetPath) {
+	// 	if err := makeFile(targetPath); err != nil {
+	// 		return fmt.Errorf("Failed make diary file. %s", err.Error())
+	// 	}
+	// }
+
+	// Open text editor
+	editorEnv := os.Getenv("EDITOR")
+	if editorEnv == "" {
+		editorEnv = "vim"
+	}
+	err = openEditor(editorEnv, targetPath)
+	if err != nil {
+		return fmt.Errorf("Failed open editor. %s", err.Error())
+	}
+	return nil
+}
+
+func diaryDirPath() string {
+	home, _ := homedir.Dir()
+	diaryDirPath := filepath.Join(home, ".config", "lipet", "_post")
+	return diaryDirPath
+}
+
+func diaryPath(targetTime time.Time, dirPath string) (string, error) {
+	year, month, day := targetTime.Date()
+	// diaryDirPath := diaryDirPath()
+	diaryPath := filepath.Join(
+		dirPath,
+		fmt.Sprintf("%02d", year),
+		fmt.Sprintf("%02d", int(month)),
+		fmt.Sprintf("%s.md", fmt.Sprintf("%02d", day)),
+	)
+	return diaryPath, nil
+}
+
+func isFileExist(fPath string) bool {
+	_, err := os.Stat(fPath)
+	return err == nil || !os.IsNotExist(err)
+}
+
+func openEditor(program string, args ...string) error {
+	c := exec.Command(program, args...)
+	c.Stdin = os.Stdin
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	return c.Run()
+}
+
+// func makeFile(fPath string) error {
+// 	err := ioutil.WriteFile(fPath, []byte(""), 0644)
+// 	if err != nil {
+// 		return fmt.Errorf("Failed make file. %v", err.Error())
+// 	}
+// 	return nil
+// }
